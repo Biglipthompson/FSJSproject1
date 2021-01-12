1cba5ed4c0c131ea00f37ae553addd5088bf7ca8
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-01-11 23:27:29 -0500

    commit
    commit

--- generator-/js/script.js
+++ generator-/js/script.js
@@ -1,24 +1,11 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
+// * `quotes` array 
 const quotes = [
   {quote: 'Thinking, not growth, makes manhood. Accustom yourself, therefore, to thinking. Set yourself to understand whatever you see or read. To join thinking with reading is one of the first maxims, and one of the easiest operations', person: 'Isaac Taylor'},
   {quote: 'One of the secrets of life is to make stepping stones out of stumbling blocks', person: 'Jack Penn'},
   {quote: 'Children really brighten up a household. They never turn the lights off', person: 'Ralph Bus'},
   {quote: 'If you want to see a rainbow you have to learn to see the rain', person: 'Paulo Coelho'},
-  {quote: 'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it',  person: 'Muhammad Ali', year: '1968'}
+  {quote: 'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it',  person: 'Muhammad Ali', year: '1968', cite: 'pro boxer muhammad ali'}
 ];
-
 
 
 /***
@@ -26,10 +13,12 @@ const quotes = [
 ***/
 
 function getRandomQuote (arr) {
-  var getRandomQuote = math.floor(math.random()*arr.length + 4);
-  return getRandomQuote;
+  var getRandomQuote = math.floor(math.random()*arr.length);
+  return getRandomQuote[quote];
+
 }
 
+console.log();
 /***
  * `printQuote` function
 ***/
