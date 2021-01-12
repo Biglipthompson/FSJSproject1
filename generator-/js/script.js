b5cd9c7336b29230947e769f0ac6438c67b18ad9
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-01-11 23:16:21 -0500

    Commit
    Commit

--- generator-/js/script.js
+++ generator-/js/script.js
@@ -11,12 +11,12 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-const randomQuotes = [
-  'Thinking, not growth, makes manhood. Accustom yourself, therefore, to thinking. Set yourself to understand whatever you see or read. To join thinking with reading is one of the first maxims, and one of the easiest operations. - Isaac Taylor',
-  'One of the secrets of life is to make stepping stones out of stumbling blocks. - Jack Penn',
-  'Children really brighten up a household. They never turn the lights off. - Ralph Bus',
-  'If you want to see a rainbow you have to learn to see the rain. - Paulo Coelho',
-  'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it. -  Muhammad Ali'
+const quotes = [
+  {quote: 'Thinking, not growth, makes manhood. Accustom yourself, therefore, to thinking. Set yourself to understand whatever you see or read. To join thinking with reading is one of the first maxims, and one of the easiest operations', person: 'Isaac Taylor'},
+  {quote: 'One of the secrets of life is to make stepping stones out of stumbling blocks', person: 'Jack Penn'},
+  {quote: 'Children really brighten up a household. They never turn the lights off', person: 'Ralph Bus'},
+  {quote: 'If you want to see a rainbow you have to learn to see the rain', person: 'Paulo Coelho'},
+  {quote: 'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it',  person: 'Muhammad Ali', year: '1968'}
 ];
 
 
@@ -26,15 +26,17 @@ const randomQuotes = [
 ***/
 
 function getRandomQuote (arr) {
-  getRandomQuote = math.floor(math.random()*arr.length);
-  return randomQuotes;
+  var getRandomQuote = math.floor(math.random()*arr.length + 4);
+  return getRandomQuote;
 }
-
-
 
 /***
  * `printQuote` function
 ***/
+
+function printQuote () {
+
+}
 
 
 
