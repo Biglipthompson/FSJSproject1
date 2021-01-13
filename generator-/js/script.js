08fd352de4a39a271d1e91988bd57f1ef3017fb1
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-01-12 21:38:26 -0500

    Commit
    Commit

--- generator-/js/script.js
+++ generator-/js/script.js
@@ -1,31 +1,40 @@
 // * `quotes` array 
 const quotes = [
-  {quote: 'Thinking, not growth, makes manhood. Accustom yourself, therefore, to thinking. Set yourself to understand whatever you see or read. To join thinking with reading is one of the first maxims, and one of the easiest operations', person: 'Isaac Taylor'},
-  {quote: 'One of the secrets of life is to make stepping stones out of stumbling blocks', person: 'Jack Penn'},
-  {quote: 'Children really brighten up a household. They never turn the lights off', person: 'Ralph Bus'},
-  {quote: 'If you want to see a rainbow you have to learn to see the rain', person: 'Paulo Coelho'},
-  {quote: 'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it',  person: 'Muhammad Ali', year: '1968', cite: 'pro boxer muhammad ali'}
+  {quote: 'Thinking, not growth, makes manhood. Accustom yourself, therefore, to thinking. Set yourself to understand whatever you see or read. To join thinking with reading is one of the first maxims, and one of the easiest operations', source: 'Isaac Taylor'},
+  {quote: 'One of the secrets of life is to make stepping stones out of stumbling blocks', source: 'Jack Penn'},
+  {quote: 'Children really brighten up a household. They never turn the lights off', source: 'Ralph Bus'},
+  {quote: 'If you want to see a rainbow you have to learn to see the rain', source: 'Paulo Coelho'},
+  {quote: 'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it', source: 'Muhammad Ali', year: '1968', citation: 'pro boxer muhammad ali'}
 ];
 
 
-/***
- * `getRandomQuote` function
-***/
-
+//This random quote function gets a random number and returns random quote
 function getRandomQuote (arr) {
-  var getRandomQuote = math.floor(math.random()*arr.length);
-  return getRandomQuote[quote];
-
+  let randomNumber = Math.floor(Math.random()*arr.length);
+  return arr[randomNumber];
 }
-
-console.log();
 /***
  * `printQuote` function
 ***/
-
 function printQuote () {
+  let quoteToPrint = getRandomQuote[quotes];
+  let HTMLstring = 
+  `<p class="quote">${quoteToPrint.quote}</p>
+  <p class="source">${quoteToPrint.source}`;
 
+  if( quoteToPrint.citation = false){
+    HTMLstring += `<span class="citation">${quoteToPrint.citation}</span>`
+  }
+  if(quoteToPrint.year = false){
+    HTMLstring += `<span class="year">${quoteToPrint.year}</span></p>`
+  }
+
+  document.getElementById('quote-box').innerHTML = HTMLstring; 
+  return quoteToPrint;
 }
+
+printQuote();
+
 
 
 
