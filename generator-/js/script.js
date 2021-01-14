85d10678ee140907dc7b7dad5f8e8bb327539dd3
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2021-01-14 18:18:38 -0500

    Commit
    Commit

--- generator-/js/script.js
+++ generator-/js/script.js
@@ -4,7 +4,7 @@ const quotes = [
   {quote: 'One of the secrets of life is to make stepping stones out of stumbling blocks', source: 'Jack Penn'},
   {quote: 'Children really brighten up a household. They never turn the lights off', source: 'Ralph Bus'},
   {quote: 'If you want to see a rainbow you have to learn to see the rain', source: 'Paulo Coelho'},
-  {quote: 'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it', source: 'Muhammad Ali', year: '1968', citation: 'pro boxer muhammad ali'}
+  {quote: 'I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it', source: 'Muhammad Ali', year: '1968', citation: 'MGM fights'}
 ];
 
 
@@ -20,15 +20,18 @@ function printQuote () {
   let quoteToPrint = getRandomQuote(quotes);
   let HTMLstring = 
   `<p class="quote">${quoteToPrint.quote}</p>
-  <p class="source">${quoteToPrint.source}`;
+  <p class="source">${quoteToPrint.source}`
 
-  if( quoteToPrint.citation = false){
-    HTMLstring += `<span class="citation">${quoteToPrint.citation}</span>`
-  }
-  if(quoteToPrint.year = false){
-    HTMLstring += `<span class="year">${quoteToPrint.year}</span></p>`
+	if (quoteToPrint.citation) {
+		HTMLstring += `<span class="citation">${quoteToPrint.citation}</span>`;
+	}
+  if (quoteToPrint.year){
+    HTMLstring += `<span class="year">${quoteToPrint.year}</span>`;
   }
 
+
+
+  HTMLstring += `</p>`
   document.getElementById('quote-box').innerHTML = HTMLstring; 
   return quoteToPrint;
 }
